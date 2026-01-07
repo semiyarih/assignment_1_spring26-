f0e87b6e59f3ec9c35091fd832d1fb2a72caab25
Hamid Semiyari <semiyari@american.edu>
2026-01-06 21:46:04 -0500

    Initial commit: Starter code and instructions

---
+++ README.md
@@ -0,0 +1,5 @@
+# Assignment 1: College Data Analysis
+## Instructions
+1. Open assignment_1.R and complete the tasks.
+2. Use relative paths for the file collegedata.csv
+3. Commit and push your final script.
---
+++ hw0.R
@@ -0,0 +1,17 @@
+
+library(tidyverse)
+
+# Task 1: Read data using a relative path
+# YOUR CODE HERE:
+college_data <- read_csv("collegedata.csv")
+
+# Task 2: Determine rows and columns
+# YOUR CODE HERE:
+
+
+# Task 3: Use tidyverse to select INSTNM, SAT_AVG, and ADM_RATE
+# YOUR CODE HERE:
+
+
+# Task 4: Create a scatterplot for SAT_AVG and ADM_RATE (Remove NA)
+# YOUR CODE HERE:
